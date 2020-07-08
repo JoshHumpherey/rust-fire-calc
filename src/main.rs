7717659570ed8b536bd7284a28d72fa24aa33469
Josh Humphrey <jhumphrey321@gmail.com>
2020-07-07 20:51:43 -0700

    Generating simulations as vectors instead of final values

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,16 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::collections::HashMap;
 use rand::Rng;
-use gnuplot::{Figure, Caption, Color};
+use plotlib::page::Page;
+use plotlib::repr::Plot;
+use plotlib::view::ContinuousView;
+use plotlib::style::{PointMarker, PointStyle};
 
 const STOCK_FILENAME: &str = "stocks.txt";
 const BOND_FILENAME: &str = "bonds.txt";
 const YEARLY_CONTRIBUTION: f64 = 50_000.0;
 const TIME_HORIZON_IN_YEARS: i32 = 40;
-const SIMULATIONS: i32 = 100_000;
+const SIMULATIONS: i32 = 100;
 const INITIAL_CAPITAL: f64 = 50_000.0;
 const STOCK_WEIGHT: f64 = 1.0;
 const BOND_WEIGHT: f64 = 1.0-STOCK_WEIGHT;
@@ -17,11 +20,22 @@ fn main() {
     let stock_hashmap = parse_data(&STOCK_FILENAME);
     let bond_hashmap = parse_data(&BOND_FILENAME);
     let simulation_results = monte_carlo_generator(&stock_hashmap, &bond_hashmap);
+
+    let test_data = simulation_results.get(0).unwrap();
+    let s1: Plot = Plot::new(test_data.to_vec()).point_style(
+        PointStyle::new()
+            .marker(PointMarker::Circle) // setting the marker to be a square
+            .colour("#DD3355"),
+    ); // and a custom colour
+
+    // The 'view' describes what set of data is drawn
+    let v = ContinuousView::new()
+        .add(s1)
+        .x_label("Investing Timeline")
+        .y_label("Net Worth");
     
-    let mut fg = Figure::new();
-    fg.axes2d()
-        .lines(&simulation_results, &simulation_results, &[Caption("A line"), Color("black")]);
-    fg.show().unwrap();
+    // A page with a single view is then saved to an SVG file
+    Page::single(&v).save("scatter.svg").unwrap();
 }
 
 fn parse_data(filename: &str) -> HashMap<usize, f64> {
@@ -37,16 +51,21 @@ fn parse_data(filename: &str) -> HashMap<usize, f64> {
     return hash_map;
 }
 
-fn simulate_investor_lifetime(stock_hashmap: &HashMap<usize, f64>, bond_hashmap: &HashMap<usize, f64>) -> f64 {
+fn simulate_investor_lifetime(stock_hashmap: &HashMap<usize, f64>, bond_hashmap: &HashMap<usize, f64>) -> Vec<(f64, f64)> {
     let limit = stock_hashmap.len();
     let mut current_portfolio_value = INITIAL_CAPITAL;
-    for _ in 0..TIME_HORIZON_IN_YEARS {
+    let mut investor_lifetime = Vec::new();
+    investor_lifetime.push((0.0, current_portfolio_value));
+
+    for i in 1..TIME_HORIZON_IN_YEARS {
         let year_index = rand::thread_rng().gen_range(0, limit);
         let stock_performance = stock_hashmap.get(&year_index).unwrap();
         let bond_performance = bond_hashmap.get(&year_index).unwrap();
         current_portfolio_value = calculate_year_performance(stock_performance, bond_performance, current_portfolio_value);
+        let tuple = (i as f64, current_portfolio_value);
+        investor_lifetime.push(tuple);
     }
-    return current_portfolio_value;
+    return investor_lifetime;
 } 
 
 fn calculate_year_performance(stock_performance: &f64, bond_performance: &f64, current_value: f64) -> f64 {
@@ -55,21 +74,13 @@ fn calculate_year_performance(stock_performance: &f64, bond_performance: &f64, c
     return stock_change + bond_change + current_value + YEARLY_CONTRIBUTION;
 }
 
-fn monte_carlo_generator(stock_hashmap: &HashMap<usize, f64>, bond_hashmap: &HashMap<usize, f64>) -> Vec<f64>{
-    let mut vec = Vec::new();
-    let mut min_val = 100000000.0;
-    let mut max_val = 0.0;
+fn monte_carlo_generator(stock_hashmap: &HashMap<usize, f64>, bond_hashmap: &HashMap<usize, f64>) -> Vec<Vec<(f64, f64)>> {
+    let mut simulation_results = Vec::new();
 
     for _ in 0..SIMULATIONS {
         let result = simulate_investor_lifetime(&stock_hashmap, &bond_hashmap);
-        if result < min_val {
-            min_val = result;
-        }
-        if result > max_val {
-            max_val = result;
-        }
-        vec.push(result);
+        simulation_results.push(result);
     }
-    println!("Worst outcome: {}, Best outcome: {}", min_val, max_val);
-    return vec;
+
+    return simulation_results;
 }
